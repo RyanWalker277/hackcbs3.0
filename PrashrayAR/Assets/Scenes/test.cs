710eceedd0669d7600292f71b59817b6d7a92d3c
RyanWalker277 <andianurag277@gmail.com>
2022-11-06 12:56:35 +0530

    Added AR

---
+++ PrashrayAR/Assets/Scenes/test.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject prefab;
+    void Start()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+        }
+    }
+}
